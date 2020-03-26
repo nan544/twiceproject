321507f0eab30d9f395c31d1adbbbae2f71e5795
jungseo <pealyteill@naver.com>
2020-03-26 10:38:33 +0900

    아이디 중복 확인 에러 해결

--- src/main/webapp/resources/include/js/common.js
+++ src/main/webapp/resources/include/js/common.js
@@ -80,9 +80,12 @@ function chkFile(item) {
  * 배열: 유효성 체크 시 필요한 정규식으로 배열을 초기화. pattern = [ 아이디 , 비밀번호, 핸드폰번호] 함수명:
  * inputVerify(배열 인덱스번호, 비교할 값, 출력영역)
  */
-var pattern = [ "((?=.*[a-zA-Z])(?=.*[0-9]).{6,10})",
-		"((?=.*[a-zA-Z])(?=.*[0-9@#$%]).{8,12})", "^\\d{3}-\\d{3,4}-\\d{4}" ];
+
 function inputVerify(index, data, printarea) {
+	
+	var pattern = [ "((?=.*[a-zA-Z])(?=.*[0-9]).{6,10})",
+		"((?=.*[a-zA-Z])(?=.*[0-9@#$%]).{8,12})", "^\\d{3}-\\d{3,4}-\\d{4}" ];
+	
 	var data_regExp = new RegExp(pattern[index]);
 	var match = data_regExp.exec($(data).val());
 	if (match == null) {
